7fa18153ec059a01de63a7d46c784bc7facd0d87
nithipat22 <wannee857@gmail.com>
2025-09-30 22:47:21 +0700

    loginv2

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -2,7 +2,9 @@ const express = require('express')
 const registerRoute = require('../controller/user/register')
 const loginRoute = require('../controller/user/login')
 
+
 const router = express.Router()
+
 
 router.use('/register', registerRoute)
 router.use('/login', loginRoute)
